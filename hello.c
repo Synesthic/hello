d8710ee06a554ef552f915838f2b857d20499462
Ryan Getty <rgetty7@gmail.com>
2023-01-27 10:29:06 -0600

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ryan Getty
+ * Date: 2023/01/31
+ *
+ * This is a "Hello-World"-like program that prints my name.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf ("Ryan Getty\n");
+
+    return 0;
+}
